fdeae2c9a7918e51eef0c9b6b141fbaf29745493
stefany <stefany.carballo@n5now.com>
2021-08-10 09:02:09 -0300

    WIP: shadows, basic animation, textures

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import {Suspense, useRef} from 'react'
 import './App.css';
+import {Canvas, useLoader, useFrame} from '@react-three/fiber'
+import {OrbitControls} from '@react-three/drei'
+import {DoubleSide, TextureLoader} from 'three'
+
+
+const Luces = ()=>{
+  return(
+    <group>
+      <ambientLight intensity={0.5}/>
+      <directionalLight castShadow position={[-4,3,3]}/>
+    </group>
+  )
+}
+
+const Cubo = ({position, color})=>{
+  const ref = useRef()
+  useFrame(({clock})=>{
+    ref.current.rotation.y +=0.1
+    ref.current.rotation.y  = Math.sin(clock.elapsedTime)
+  })
+  const texture = useLoader(TextureLoader, process.env.PUBLIC_URL +'/box.jpg')
+  return(
+    <mesh ref={ref} castShadow position={position}>
+      <boxGeometry attach="geometry" args={[1,1,1]}/>
+      <meshPhysicalMaterial attach="material" color={color} map={texture}/>
+    </mesh>
+  )
+}
+
+const Floor = ()=>{
+  return(
+    <mesh receiveShadow rotation={[-Math.PI/2, 0, 0]} position={[0,-0.5,0]}>
+      <planeBufferGeometry attach="geometry" args={[10,10]}/>
+      <meshPhysicalMaterial attach="material" color="green" side={DoubleSide}/>
+    </mesh>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas shadows>
+        <axesHelper/>
+        <Luces/>
+        <OrbitControls/>
+        <Suspense fallback={null}>
+          <Floor/>
+          <Cubo position={[-2.5, 1.5,0]} color="yellow"/>
+          <Cubo position={[0, 1,0]} color="#81cefb"/>
+          <Cubo position={[2.5, 0.5,0]} color="red"/>
+        </Suspense>
+
+      </Canvas>
     </div>
   );
 }
